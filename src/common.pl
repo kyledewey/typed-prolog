f5ac440bff836e8b5145ed099a0f8bf793fdc3cc
Kyle Dewey <kyledewey@cs.ucsb.edu>
2015-03-20 10:28:15 -0700

    Implemented foldRight and flatMap.

--- src/common.pl
+++ src/common.pl
@@ -1,5 +1,5 @@
 module(common, [map/3, filter/3, foldLeft/4, forall/2,
-                setContains/2], [pair, option]).
+                setContains/2, flatMap/3, foldRight/4], [pair, option]).
 
 datadef(pair, [A, B], [pair(A, B)]).
 datadef(option, [A], [some(A), none]).
@@ -10,6 +10,15 @@ map([HA|TA], F, [HB|TB]) :-
         call(F, HA, HB),
         map(TA, F, TB).
 
+clausedef(flatMap, [A, B], [list(A), relation([A, list(B)]), list(B)]).
+flatMap(List, Relation, ResultList) :-
+        foldRight(
+            List, [],
+            lambda([Cur, Accum, NewAccum],
+                (call(Relation, Cur, CurList),
+                 append(CurList, Accum, NewAccum))),
+            ResultList).
+
 clausedef(filter, [A], [list(A), relation([A]), list(A)]).
 filter([], _, []).
 filter([H|T], R, ResultList) :-
@@ -17,6 +26,12 @@ filter([H|T], R, ResultList) :-
             ResultList = [H|Rest];
             ResultList = Rest),
         filter(T, R, Rest).
+
+clausedef(foldRight, [A, B], [list(A), B, relation([A, B, B]), B]).
+foldRight([], Accum, _, Accum).
+foldRight([H|T], Accum, Relation, Result) :-
+        foldRight(T, Accum, Relation, TailAccum),
+        call(Relation, H, TailAccum, Result).
 
 clausedef(foldLeft, [A, B], [list(A), B, relation([B, A, B]), B]).
 foldLeft([], Accum, _, Accum).
@@ -36,3 +51,4 @@ setContains([H|_], Item) :-
         H == Item.
 setContains([_|T], Item) :-
         setContains(T, Item).
+
