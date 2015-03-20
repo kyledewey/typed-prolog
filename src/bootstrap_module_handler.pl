b0c119e084df0da440befa3ad4ac004d9b6563c4
Kyle Dewey <kyledewey@cs.ucsb.edu>
2015-03-20 15:51:01 -0700

    Checks for duplicate constructors.

--- src/bootstrap_module_handler.pl
+++ src/bootstrap_module_handler.pl
@@ -1,6 +1,6 @@
 module(bootstrap_module_handler, [], []).
 
-use_module('common.pl', [notMember/2, foldLeft/4], []).
+use_module('common.pl', [notMember/2, foldLeft/4, flatMap/3, map/3, forall/2], [pair]).
 use_module('bootstrap_syntax.pl', [loadFile/2],
                                   [op, exp, expLhs, term, bodyPairOp, body, type, defclause,
                                    typeConstructor, defdata, clauseclause, defglobalvar,
@@ -24,6 +24,50 @@ yolo_UNSAFE_absolute_file_name(RelativeName, RelativeTo, AbsoluteName) :-
         absolute_file_name(RelativeName, AbsoluteName, [relative_to(RelativeTo)]),
         access_file(AbsoluteName, read).
 
+clausedef(constructorsInDataDefs, [], [list(defdata), list(atom)]).
+constructorsInDataDefs(DataDefs, Constructors) :-
+        flatMap(DataDefs,
+                lambda([defdata(_, _, TypeConstructors), CurConstructors],
+                       map(TypeConstructors,
+                           lambda([typeConstructor(Name, _), Name], true),
+                           CurConstructors)),
+                Constructors).
+
+clausedef(allImportedConstructors, [], [list(loadedModule),
+                                        atom, % absolute filename of original module
+                                        list(def_use_module),
+                                        list(atom)]).
+allImportedConstructors(LoadedModules, RelativeTo, UsesModules, Constructors) :-
+        flatMap(UsesModules,
+                lambda([def_use_module(RelFilename, _, ImportedTypes), CurConstructors],
+                       (yolo_UNSAFE_absolute_file_name(RelFilename, RelativeTo, AbsFile),
+                        importedConstructors(LoadedModules, AbsFile, ImportedTypes,
+                                             CurConstructors))),
+                Constructors).
+
+% Assumes that all dependencies have been loaded in.
+clausedef(importedConstructors, [], [list(loadedModule), % everything loaded in so far
+                                     atom, % absolute filename of imported module
+                                     list(atom), % imported types
+                                     list(atom)]). % imported constructors
+importedConstructors(LoadedModules, Filename, ImportedTypes, Constructors) :-
+        % get the corresponding module
+        member(loadedModule(Filename, _, ModuleFile), LoadedModules),
+        ModuleFile = loadedFile(defmodule(_, _, ExportedTypes), _, DataDefs, _, _, _),
+
+        % ensure the module exports the types we want
+        forall(ImportedTypes, lambda([ImportedType], member(ImportedType, ExportedTypes))),
+
+        % get their corresponding data defs
+        map(ImportedTypes,
+            lambda([TypeName, Probe],
+                   (Probe = defdata(TypeName, _, _),
+                    member(Probe, DataDefs))),
+            ImportedDataDefs),
+
+        % get the corresponding constructors
+        constructorsInDataDefs(ImportedDataDefs, Constructors).
+
 clausedef(directLoadModule, [], [atom, % absolute filename
                                  list(loadedModule), % already loaded modules
                                  list(atom), % in progress loading
@@ -31,12 +75,25 @@ clausedef(directLoadModule, [], [atom, % absolute filename
 directLoadModule(FileName, AlreadyLoaded, InProgress,
                  [loadedModule(FileName, ModuleId, LoadedFile)|RestLoaded]) :-
         loadFile(FileName, LoadedFile),
-        LoadedFile = loadedFile(_, UsesModules, _, _, _, _),
+        LoadedFile = loadedFile(_, UsesModules, DataDefs, _, _, _),
+
+        % perform the actual loading
         foldLeft(UsesModules, AlreadyLoaded,
                  lambda([CurLoaded, def_use_module(CurFileName, _, _), TempLoaded],
-                        loadModule(CurFileName, FileName, CurLoaded, InProgress, TempLoaded)),
+                         loadModule(CurFileName, FileName, CurLoaded, InProgress, TempLoaded)),
                  RestLoaded),
+
+        % ensure we haven't introduced any duplicate constructors
+        allImportedConstructors(TempLoaded, FileName, UsesModules, ImportedConstructors),
+        constructorsInDataDefs(DataDefs, LocalConstructors),
+        append(ImportedConstructors, LocalConstructors, AllConstructors),
+        is_set(AllConstructors),
+
         freshModuleId(ModuleId).
+
+datadef(renaming, [], [renaming(list(pair(pair(atom, int), atom)), % for clauses
+                                list(pair(atom, atom)), % for types
+                                list(pair(atom, atom)))]). % for constructors
 
 clausedef(loadModule, [], [atom, % possibly relative filename
                            atom, % relative to another file
