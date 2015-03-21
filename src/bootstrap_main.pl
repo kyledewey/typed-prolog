f7ff208df59a4f48695ae94132fcd3531caeb8b7
Kyle Dewey <kyledewey@cs.ucsb.edu>
2015-03-21 14:04:00 -0700

    Have code written and hooked up for writing, though untested.

--- src/bootstrap_main.pl
+++ src/bootstrap_main.pl
@@ -3,9 +3,11 @@ module(bootstrap_main, [], []).
 use_module('bootstrap_module_handler.pl', [handleModules/5], []).
 use_module('bootstrap_typechecker.pl', [typecheckClauses/4], []).
 use_module('bootstrap_translator.pl', [translateClauses/3], [engine_type]).
+use_module('bootstrap_printer.pl', [writeTranslatedClauses/2], []).
 
-clausedef(processFile, [], [atom]).
-processFile(InputFile) :-
+clausedef(processFile, [], [atom, engine_type, atom]).
+processFile(InputFile, Engine, OutputFile) :-
         handleModules(InputFile, DataDefs, ClauseDefs, GlobalVarDefs, Clauses), !,
         typecheckClauses(DataDefs, ClauseDefs, GlobalVarDefs, Clauses), !,
-        translateClauses(Clauses, swipl, NewClauses), !.
+        translateClauses(Clauses, Engine, TranslatedClauses), !,
+        writeTranslatedClauses(TranslatedClauses, OutputFile), !.
---
+++ src/bootstrap_printer.pl
@@ -0,0 +1,87 @@
+module(bootstrap_printer, [writeTranslatedClauses/2], []).
+
+use_module('common.pl', [map/3], []).
+use_module('bootstrap_syntax.pl', [],
+                                  [op, exp, expLhs, term, bodyPairOp, body, type, defclause,
+                                   typeConstructor, defdata, clauseclause, defglobalvar,
+                                   defmodule, def_use_module, loadedFile]).
+use_module('io.pl', [writeClauses/2], []).
+
+% Handles writing to files.  Assumes that translation has already occurred, so that
+% it won't encounter getvar, setvar, lambda, or higher order calls.
+
+% A whole lot of this file is unsafe because we are going down to raw Prolog
+% terms, for which we no not have an associated type.
+
+clausedef(translateOp, [], [op, atom]).
+translateOp(plus, '+').
+translateOp(minus, '-').
+translateOp(mul, '*').
+translateOp(div, '/').
+translateOp(op_min, min).
+translateOp(op_max, max).
+
+clausedef(translateBodyPairOp, [], [bodyPairOp, atom]).
+translateBodyPairOp(and, ',').
+translateBodyPairOp(or, ';').
+translateBodyPairOp(implies, '->').
+
+clausedef(yolo_UNSAFE_translate_exp, [A], [exp, A]).
+yolo_UNSAFE_translate_exp(exp_var(X), X).
+yolo_UNSAFE_translate_exp(exp_num(N), N).
+yolo_UNSAFE_translate_exp(binop(E1, Op, E2), Output) :-
+        translateOp(Op, NewOp),
+        yolo_UNSAFE_translate_exp(E1, NewE1),
+        yolo_UNSAFE_translate_exp(E2, NewE2),
+        Output =.. [NewOp, NewE1, NewE2].
+
+clausedef(yolo_UNSAFE_translate_exp_lhs, [A], [expLhs, A]).
+yolo_UNSAFE_translate_exp_lhs(lhs_var(X), X).
+yolo_UNSAFE_translate_exp_lhs(lhs_num(N), N).
+
+clausedef(translateTerms, [A], [list(term), list(A)]).
+translateTerms(Terms, Result) :-
+        map(Terms,
+            lambda([T, R], yolo_UNSAFE_translate_term(T, R)),
+            Result).
+
+clausedef(yolo_UNSAFE_translate_term, [A], [term, A]).
+yolo_UNSAFE_translate_term(term_var(X), X).
+yolo_UNSAFE_translate_term(term_num(N), N).
+% lambdas have been translated away
+yolo_UNSAFE_translate_term(term_constructor(Name, Terms), Result) :-
+        translateTerms(Terms, NewTerms),
+        Result =.. [Name|NewTerms].
+
+clausedef(yolo_UNSAFE_translate_body, [A], [body, A]).
+yolo_UNSAFE_translate_body(body_is(Lhs, Exp), Result) :-
+        yolo_UNSAFE_translate_exp_lhs(Lhs, NewLhs),
+        yolo_UNSAFE_translate_exp(Exp, NewExp),
+        Result =.. [is, NewLhs, NewExp].
+% setvar and getvar has been translated away
+yolo_UNSAFE_translate_body(bodyPair(B1, Op, B2), Result) :-
+        translateBodyPairOp(Op, NewOp),
+        yolo_UNSAFE_translate_body(B1, NewB1),
+        yolo_UNSAFE_translate_body(B2, NewB2),
+        Result =.. [NewOp, NewB1, NewB2].
+% higher order calls have been translated away
+yolo_UNSAFE_translate_body(firstOrderCall(Name, Terms), Result) :-
+        translateTerms(Terms, NewTerms),
+        Result =.. [Name|NewTerms].
+
+clausedef(yolo_UNSAFE_translate_clause, [A], [clauseclause, A]).
+yolo_UNSAFE_translate_clause(clauseclause(Name, Params, Body), NewClause) :-
+        translateTerms(Params, NewParams),
+        yolo_UNSAFE_translate_body(Body, NewBody),
+        Head =.. [Name|NewParams],
+        (NewBody == true ->
+            (NewClause = Head);
+            (NewClause =.. [':-', Head, NewBody])).
+
+clausedef(writeTranslatedClauses, [], [list(clauseclause), atom]).
+writeTranslatedClauses(Clauses, Filename) :-
+        map(Clauses,
+            lambda([Clause, NewClause],
+                   yolo_UNSAFE_translate_clause(Clause, NewClause)),
+            TranslatedClauses),
+        writeClauses(TranslatedClauses, Filename).
