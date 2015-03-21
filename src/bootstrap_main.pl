7066778cfd60c98894df5c55a5f082e89dcfc4b8
Kyle Dewey <kyledewey@cs.ucsb.edu>
2015-03-21 14:19:51 -0700

    Added stubs for testing specific engines.

--- src/bootstrap_main.pl
+++ src/bootstrap_main.pl
@@ -1,4 +1,4 @@
-module(bootstrap_main, [], []).
+module(bootstrap_main, [processFileForSwipl/2, processFileForGnuProlog/2], []).
 
 use_module('bootstrap_module_handler.pl', [handleModules/5], []).
 use_module('bootstrap_typechecker.pl', [typecheckClauses/4], []).
@@ -11,3 +11,13 @@ processFile(InputFile, Engine, OutputFile) :-
         typecheckClauses(DataDefs, ClauseDefs, GlobalVarDefs, Clauses), !,
         translateClauses(Clauses, Engine, TranslatedClauses), !,
         writeTranslatedClauses(TranslatedClauses, OutputFile), !.
+
+% these stubs are needed so we can call them directly without having
+% to manually lookup to see what they got translated to due to modules.
+clausedef(processFileForSwipl, [], [atom, atom]).
+processFileForSwipl(InputFile, OutputFile) :-
+        processFile(InputFile, swipl, OutputFile).
+
+clausedef(processFileForGnuProlog, [], [atom, atom]).
+processFileForGnuProlog(InputFile, OutputFile) :-
+        processFile(InputFile, gnuprolog, OutputFile).
