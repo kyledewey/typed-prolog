4ae014cca88f7765cfe6523eda90cd7bfab00333
Kyle Dewey <kyledewey@cs.ucsb.edu>
2015-03-21 10:57:57 -0700

    Stub for main now goes through the module translator.

--- src/bootstrap_main.pl
+++ src/bootstrap_main.pl
@@ -1,9 +1,10 @@
 module(bootstrap_main, [], []).
 
-use_module('bootstrap_syntax.pl', [loadFile/2], [loadedFile]).
+use_module('bootstrap_module_handler.pl', [handleModules/5], []).
 use_module('bootstrap_typechecker.pl', [typecheckClauses/4], []).
+
 
 clausedef(processFile, [], [atom]).
 processFile(InputFile) :-
-        loadFile(InputFile, loadedFile(_, _, DataDefs, ClauseDefs, GlobalVarDefs, Clauses)), !,
+        handleModules(InputFile, DataDefs, ClauseDefs, GlobalVarDefs, Clauses), !,
         typecheckClauses(DataDefs, ClauseDefs, GlobalVarDefs, Clauses), !.
