72f4f91d6a7deec826bb16b8b92d8d6818afca8b
Mehmet Emre <maemre2@gmail.com>
2017-10-16 16:35:29 -0700

    Add more tests for the atom `<=`

--- src/tests/atom_lte.pl
+++ src/tests/atom_lte.pl
@@ -1,5 +1,14 @@
 module(atom_lte, [], []).
 
+% test at the end of the clause
 clausedef(mkAtom, [], [atom]).
 mkAtom(Atom) :- Atom = '<='.
 mkAtom(Atom) :- Atom = 'div'.
+
+% test within conjunction
+clausedef(mkAtom2, [], [atom]).
+mkAtom2(Atom) :- Atom = '<=', Atom = '<='.
+
+% test within disjunction
+clausedef(mkAtom3, [], [atom]).
+mkAtom3(Atom) :- Atom = '<='; Atom = 'div'.
